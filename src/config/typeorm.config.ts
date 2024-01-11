1bb5f77f6b2f987fcc6f86d8ea27b1d7036fdfd0
Jae Min Ko <jvc123456@khu.ac.kr>
2024-01-11 22:47:02 +0900

    feat(typeorm): add migration setting

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -7,23 +7,24 @@ import { Question } from '../entities/question.entity';
 const config : ConfigService = new ConfigService();
 
 export const typeORMConfig : TypeOrmModuleOptions = {
-        type: 'postgres',
-        host: config.get<string>('DATABASE_HOST'),
-        port: config.get<number>('DATABASE_PORT'),
-        username: config.get<string>('DATABASE_USERNAME'),
-        password: config.get<string>('DATABASE_PASSWORD'),
-        database: config.get<string>('DATABASE_DATABASE'),
-        entities: [Question, Answer],
-        synchronize : false
+  type: 'postgres',
+  host: config.get<string>('DATABASE_HOST'),
+  port: config.get<number>('DATABASE_PORT'),
+  username: config.get<string>('DATABASE_USERNAME'),
+  password: config.get<string>('DATABASE_PASSWORD'),
+  database: config.get<string>('DATABASE_DATABASE'),
+  entities: [Question, Answer],
+  synchronize : false,
+  keepConnectionAlive: true,
 }
 
 export const testTypeORMConfig : TypeOrmModuleOptions = {
-        type: 'postgres',
-        host: config.get<string>('TEST_DATABASE_HOST'),
-        port: config.get<number>('TEST_DATABASE_PORT'),
-        username: config.get<string>('TEST_DATABASE_USERNAME'),
-        password: config.get<string>('TEST_DATABASE_PASSWORD'),
-        database: config.get<string>('TEST_DATABASE_DATABASE'),
-        entities: [Question, Answer],
-        synchronize : true
+  type: 'postgres',
+  host: config.get<string>('TEST_DATABASE_HOST'),
+  port: config.get<number>('TEST_DATABASE_PORT'),
+  username: config.get<string>('TEST_DATABASE_USERNAME'),
+  password: config.get<string>('TEST_DATABASE_PASSWORD'),
+  database: config.get<string>('TEST_DATABASE_DATABASE'),
+  entities: [Question, Answer],
+  synchronize : true
 }
