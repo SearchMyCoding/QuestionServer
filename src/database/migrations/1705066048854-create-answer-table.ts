aff59eb51dee04d3eb4e823f07a483d9009854b4
Jae Min Ko <jvc123456@khu.ac.kr>
2024-01-12 23:18:13 +0900

    feat(migration): add migration for creating answer table

---
+++ src/database/migrations/1705066048854-create-answer-table.ts
@@ -0,0 +1,36 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createAnswerTable1705066048854 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "answer",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                    },
+                    {
+                        name: "answerType",
+                        type: "varchar",
+                        length: "50"
+                    },
+                    {
+                        name: "contents",
+                        type: "varchar",
+                        length: "500",
+                        isUnique: true
+                    },
+                ]
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("answer");
+    }
+
+}
