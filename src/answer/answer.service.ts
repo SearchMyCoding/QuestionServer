2aba3354c82ced1670ef0fb48a598e657423460a
Jae Min Ko <jvc123456@khu.ac.kr>
2024-01-19 23:38:14 +0900

    refactor(answer): refactoring about answer service
    typeorm version update

--- src/answer/answer.service.ts
+++ src/answer/answer.service.ts
@@ -2,33 +2,41 @@ import { UpdateAnswerDto } from '../dto/UpdateAnswer.dto';
 import { CreateAnswerDto } from '../dto/CreateAnswer.dto';
 import { Answer } from '../entities/answer.entity';
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Question } from '../entities/question.entity';
 
 @Injectable()
 export class AnswerService {
+    private answerRepository : Repository<Answer>;
+    private questionRepository : Repository<Question>;
     constructor(
-        @InjectRepository(Answer)
-        private readonly answerRepository : Repository<Answer>,
-        @InjectRepository(Question)
-        private readonly questionRepository : Repository<Question>
-    ){}
+        private readonly datasource : DataSource,
+    ){
+        this.answerRepository = this.datasource.getRepository("answer");
+        this.questionRepository = this.datasource.getRepository("question");
+    }
 
     async getAllAnswer() : Promise<Answer[]>{
         return await this.answerRepository.find();
     }
 
     async getOneAnswer(answerId : number) : Promise<Answer>{
-        const FoundAnswer : Answer = await this.answerRepository.findOne({id : answerId});
+        const FoundAnswer : Answer = await this.answerRepository.findOne({
+            where : {
+                id: answerId,
+            }
+        });
         if(!FoundAnswer)
             throw new NotFoundException(`Answer with Id ${answerId} is not found.`);
         return FoundAnswer;
     }
 
     async getAnswerAboutQuestion(questionId : number) : Promise<Answer[]>{
-        const FoundQuestion : Question = await this.questionRepository.findOne({id:questionId},{
-            relations : ['answer']
+        const FoundQuestion : Question = await this.questionRepository.findOne({
+            where : {
+                id: questionId
+            },
+            relations: ["answer"]
         });
         if(!FoundQuestion)
             throw new NotFoundException(`Question with Id ${questionId} is not found`);
@@ -38,8 +46,11 @@ export class AnswerService {
 
     async createAnswer(createAnswerDto : CreateAnswerDto) : Promise<void>{
         const {answerType, questionId, contents} = createAnswerDto;
-        const question : Question = await this.questionRepository.findOne({id:questionId},{
-            relations : ['answer']
+        const question : Question = await this.questionRepository.findOne({
+            where : {
+                id: questionId
+            },
+            relations: ["answer"]
         });
         const newAnswer : Answer = this.answerRepository.create({
             answerType : answerType,
@@ -47,7 +58,9 @@ export class AnswerService {
             contents : contents
         });
         await this.answerRepository.insert(newAnswer);
-        const answer : Answer = await this.answerRepository.findOne({contents : contents});
+        const answer : Answer = await this.answerRepository.findOne({
+            where: { contents }
+        });
         
         question.answer.push(answer);
         await this.questionRepository.save(question);
