dbebd5dc354a9c67db1cee1effda7b3cf39caf0f
Jae Min Ko <jvc123456@khu.ac.kr>
2024-01-20 23:27:19 +0900

    feat(question): add getRandomQuestionIds in question

--- src/question/question.service.ts
+++ src/question/question.service.ts
@@ -1,23 +1,28 @@
 import { UpdateQuestionDto } from '../dto/UpdateQuestion.dto';
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
 import { CreateQuestionDto } from '../dto/CreateQuestion.dto';
 import { Question } from '../entities/question.entity';
 
 @Injectable()
 export class QuestionService {
+    private readonly questionRepository : Repository<Question>;
     constructor(
-        @InjectRepository(Question)
-        private readonly questionRepository : Repository<Question>
-    ){}
+        private readonly datasource : DataSource        
+    ){
+        this.questionRepository = this.datasource.getRepository("question");
+    }
 
     async getAllQuestions() : Promise<Question[]>{
         return await this.questionRepository.find();
     }
 
     async getOneQuestion(questionId : number) : Promise<Question>{
-        const FoundQuestion : Question = await this.questionRepository.findOne({id : questionId});
+        const FoundQuestion : Question = await this.questionRepository.findOne({
+            where: {
+                id: questionId
+            }
+        });
         if(!FoundQuestion)
             throw new NotFoundException(`Question with Id ${questionId} is not found.`);
         return FoundQuestion;
@@ -51,5 +56,32 @@ export class QuestionService {
             throw err;
         }
         await this.questionRepository.update({id : questionId},updateQuestionData);
+    }
+
+    async getRandomQuestionIds(amountOfQuestion : number){
+        const isInteger : boolean = Number.isInteger(amountOfQuestion);
+
+        if(!isInteger){
+            throw new BadRequestException(`${amountOfQuestion} is not Integer.`);
+        }
+
+        const isNegative : boolean = amountOfQuestion < 0;
+
+        if(isNegative){
+            throw new BadRequestException(`${amountOfQuestion} is negative Integer.`);
+        }
+
+        const maximum : number = await this.questionRepository.count();
+        const indexList : number[] = Array.from({length : maximum}, (_, index) => index);
+        const result : number[] = [];
+
+        while(result.length < amountOfQuestion){
+            const randomNumber = Math.floor(Math.random() * (indexList.length + 1));
+            result.push(indexList[randomNumber]);
+            indexList[randomNumber] = indexList[indexList.length - 1];
+            indexList.pop();
+        }
+
+        return result;
     }
 }
