e1f5ac3e5a95a6d5105d5d95b66e3092dab80420
Jae Min Ko <jvc123456@khu.ac.kr>
2024-01-31 22:51:01 +0900

    refactor(type): add type for method

--- src/interfaces/mbti.interface.ts
+++ src/interfaces/mbti.interface.ts
@@ -2,7 +2,7 @@ import { IBaseTestService } from "src/constants/test";
 import { ANSWER_TYPE, CHOICE_TYPE, MBTI_SINGLE_TEMPLATE_TYPE, MBTI_TYPE, QUESTION_AND_ANSWER_TYPE, QUESTION_SENDER_TYPE, QUESTION_TYPE } from "src/constants/type";
 
 export interface IMbtiTestService extends IBaseTestService{
-  addQuestionAndAnswers(question: QUESTION_SENDER_TYPE, answers: CHOICE_TYPE<MBTI_SINGLE_TEMPLATE_TYPE>[]): void;
+  addQuestionAndAnswers<T extends MBTI_SINGLE_TEMPLATE_TYPE>(question: QUESTION_SENDER_TYPE, answers: [CHOICE_TYPE<T>, CHOICE_TYPE<T>]): void;
   generateRandomTest(numQuestion: number): QUESTION_AND_ANSWER_TYPE<MBTI_SINGLE_TEMPLATE_TYPE>[];
   calculateResult(choices: ANSWER_TYPE<MBTI_SINGLE_TEMPLATE_TYPE>[]): MBTI_TYPE;
 }
