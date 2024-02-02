5d6a56222f20b7b53efc907ce27807c40a4287dd
Jae Min Ko <jvc123456@khu.ac.kr>
2024-02-02 17:31:34 +0900

    style(type): change folder and name

--- src/constants/test.ts
+++
@@ -1,5 +0,0 @@
-export interface IBaseTestService{
-  addQuestionAndAnswers(question: any, answers: any[]): void;
-  generateRandomTest(numTests: number): any[];
-  calculateResult(choices: any[]): any;
-}
--- src/interfaces/mbti.interface.ts
+++ src/interfaces/mbti.interface.ts
@@ -1,5 +1,5 @@
-import { IBaseTestService } from "src/constants/test";
-import { ANSWER_TYPE, CHOICE_TYPE, MBTI_SINGLE_TEMPLATE_TYPE, MBTI_TYPE, QUESTION_AND_ANSWER_TYPE, QUESTION_SENDER_TYPE, QUESTION_TYPE } from "src/constants/type";
+import { IBaseTestService } from "src/interfaces/test.interface";
+import { ANSWER_TYPE, CHOICE_TYPE, MBTI_SINGLE_TEMPLATE_TYPE, MBTI_TYPE, QUESTION_AND_ANSWER_TYPE, QUESTION_SENDER_TYPE } from "src/constants/mbti";
 
 export interface IMbtiTestService extends IBaseTestService{
   addQuestionAndAnswers<T extends MBTI_SINGLE_TEMPLATE_TYPE>(question: QUESTION_SENDER_TYPE, answers: [CHOICE_TYPE<T>, CHOICE_TYPE<T>]): void;
