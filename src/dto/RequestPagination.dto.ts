c7afa7eacd2f1f1d59e9866bdac9bd54ee708fbe
Jae Min Ko <jvc123456@khu.ac.kr>
2024-02-22 23:23:41 +0900

    feat(swagger): add swagger fot question

--- src/dto/RequestPagination.dto.ts
+++ src/dto/RequestPagination.dto.ts
@@ -1,16 +1,28 @@
+import { ApiProperty, PartialType } from "@nestjs/swagger";
 import { IsInt, IsOptional, IsPositive, IsString } from "class-validator";
 import { ORDER_BY_TYPE } from "src/constants/pagination.constant";
 
 export class RequestPaginationDto {
+  @ApiProperty({
+    required: false,
+    type: Number
+  })
   @IsInt()
   @IsOptional()
-  skip? : number;
+  skip?: number;
 
+  @ApiProperty({
+    required: false,
+    type: Number
+  })
   @IsPositive()
   @IsInt()
   @IsOptional()
   limit?: number;
 
+  @ApiProperty({
+    required: false
+  })
   @IsString()
   @IsOptional()
   sort?: ORDER_BY_TYPE
