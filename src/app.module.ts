ce1d0f2120e378b9bdfe98115f462242511a5d83
Jae Min Ko <jvc123456@khu.ac.kr>
2024-04-20 22:13:14 +0900

    fix(typeorm-config): fix config for TypeOrm about entity and config module
    fix #17

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { typeORMConfig } from 'src/config/typeorm.config';
 import { QuestionModule } from 'src/question/question.module';
 import { AnswerModule } from 'src/answer/answer.module';
@@ -10,7 +10,10 @@ import { AnswerModule } from 'src/answer/answer.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot(typeORMConfig),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => await typeORMConfig(config)
+    }),
     QuestionModule,
     AnswerModule,
   ],
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,31 +1,30 @@
 import { ConfigService } from '@nestjs/config';
-import 'dotenv/config';
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
-import { Answer } from 'src/entities/answer.entity';
-import { Question } from 'src/entities/question.entity';
 
-const config : ConfigService = new ConfigService();
+export const typeORMConfig = async (config: ConfigService): Promise<TypeOrmModuleOptions> => {
+  return {
+    type: 'postgres',
+    host: config.get<string>('DATABASE_HOST'),
+    port: config.get<number>('DATABASE_PORT'),
+    username: config.get<string>('DATABASE_USERNAME'),
+    password: config.get<string>('DATABASE_PASSWORD'),
+    database: config.get<string>('DATABASE_DATABASE'),
+    entities: ['dist/src/entities/*.js'],
+    synchronize : false,
+    keepConnectionAlive: true,
+  }
+};
 
-export const typeORMConfig : TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: config.get<string>('DATABASE_HOST'),
-  port: config.get<number>('DATABASE_PORT'),
-  username: config.get<string>('DATABASE_USERNAME'),
-  password: config.get<string>('DATABASE_PASSWORD'),
-  database: config.get<string>('DATABASE_DATABASE'),
-  entities: [Question, Answer],
-  synchronize : false,
-  keepConnectionAlive: true,
-}
-
-export const testTypeORMConfig : TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: config.get<string>('TEST_DATABASE_HOST'),
-  port: config.get<number>('TEST_DATABASE_PORT'),
-  username: config.get<string>('TEST_DATABASE_USERNAME'),
-  password: config.get<string>('TEST_DATABASE_PASSWORD'),
-  database: config.get<string>('TEST_DATABASE_DATABASE'),
-  entities: [Question, Answer],
-  logging: true,
-  synchronize : true
-}
+export const testTypeORMConfig = async (config: ConfigService): Promise<TypeOrmModuleOptions> => {
+  return {
+    type: 'postgres',
+    host: config.get<string>('TEST_DATABASE_HOST'),
+    port: config.get<number>('TEST_DATABASE_PORT'),
+    username: config.get<string>('TEST_DATABASE_USERNAME'),
+    password: config.get<string>('TEST_DATABASE_PASSWORD'),
+    database: config.get<string>('TEST_DATABASE_DATABASE'),
+    entities: ['dist/src/entities/*.js'],
+    logging: true,
+    synchronize : true
+  }
+};
