bde9e4e38a1a5c0d1a70d42314f29bc72aaa61ba
Jae Min Ko <jvc123456@khu.ac.kr>
2024-06-11 23:59:01 +0900

    chore(main.ts): change data type

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,26 @@
-import { validationConfig } from 'src/config/validation.config'
-import { SwaggerConfig } from 'src/config/swagger.config'
-import { INestApplication, ValidationPipe } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
-import { NestFactory } from '@nestjs/core'
-import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger'
-import { AppModule } from 'src/app.module'
-import { LoggerInterceptor } from 'src/logger/logger.interceptor'
+import { validationConfig } from 'src/config/validation.config';
+import { SwaggerConfig } from 'src/config/swagger.config';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from 'src/app.module';
+import { LoggerInterceptor } from 'src/logger/logger.interceptor';
 
 async function bootstrap() {
-  const app: INestApplication = await NestFactory.create(AppModule)
-  const config: ConfigService = app.get<ConfigService>(ConfigService)
+  const app: INestApplication = await NestFactory.create(AppModule);
+  const config: ConfigService = app.get<ConfigService>(ConfigService);
 
-  const swaggerPath: string = config.get<string>('SWAGGER_PATH')
-  const port: string = config.get<string>('SERVER_PORT')
-  const document: OpenAPIObject = SwaggerModule.createDocument(app, SwaggerConfig)
+  const swaggerPath: string = config.get<string>('SWAGGER_PATH');
+  const port: number = config.get<number>('SERVER_PORT');
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, SwaggerConfig);
 
-  app.useGlobalPipes(new ValidationPipe(validationConfig))
-  app.useGlobalInterceptors(new LoggerInterceptor())
+  app.useGlobalPipes(new ValidationPipe(validationConfig));
+  app.useGlobalInterceptors(new LoggerInterceptor());
 
-  SwaggerModule.setup(swaggerPath, app, document)
+  SwaggerModule.setup(swaggerPath, app, document);
 
-  await app.listen(port)
+  await app.listen(port);
 }
 
-bootstrap()
+bootstrap();
