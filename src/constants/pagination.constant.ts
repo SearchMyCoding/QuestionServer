e1a5cbdf5ab503548f9b20a56d31a6a722f0cb6d
Jae Min Ko <jvc123456@khu.ac.kr>
2024-06-13 23:18:04 +0900

    style(prettier): apply prettier about constants

--- src/constants/pagination.constant.ts
+++ src/constants/pagination.constant.ts
@@ -1,27 +1,21 @@
 const PAGINATION = {
-  DEFAULT: "DEFAULT",
-  SEARCH: "SEARCH"
+  DEFAULT: 'DEFAULT',
+  SEARCH: 'SEARCH',
 } as const;
 
 const ORDER_BY = {
-  ASC: "ASC",
-  DESC: "DESC"
+  ASC: 'ASC',
+  DESC: 'DESC',
 } as const;
 
 const PAGINATION_DEFAULT_VALUE = {
   skip: 0,
   limit: 10,
   sort: ORDER_BY.ASC,
-  afterBy: 1
+  afterBy: 1,
 } as const;
 
-type PAGINATION_TYPE = typeof PAGINATION[keyof typeof PAGINATION];
-type ORDER_BY_TYPE = typeof ORDER_BY[keyof typeof ORDER_BY];
+type PAGINATION_TYPE = (typeof PAGINATION)[keyof typeof PAGINATION];
+type ORDER_BY_TYPE = (typeof ORDER_BY)[keyof typeof ORDER_BY];
 
-export {
-  PAGINATION,
-  PAGINATION_DEFAULT_VALUE,
-  ORDER_BY,
-  PAGINATION_TYPE,
-  ORDER_BY_TYPE
-}
+export { PAGINATION, PAGINATION_DEFAULT_VALUE, ORDER_BY, PAGINATION_TYPE, ORDER_BY_TYPE };
